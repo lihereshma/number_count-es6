a2f559e3d2468ce5fa9b7d0eaf23134e5bd6dd07
lreshmaprdxn <reshmal@prdxn.com>
2020-05-05 11:59:05 +0530

    js end

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,12 @@
-/* Author: 
+/* Author:Reshma */
+const number = document.querySelector('.number');
+var count = 0;
 
-*/
+const increment  = document.getElementById('increment');
+const decrement  = document.getElementById('decrement');
 
+const addNumber = () => number.innerHTML = ++count;
+const lowerNumber = () => number.innerHTML = --count;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+increment.addEventListener('click', addNumber);
+decrement.addEventListener('click', lowerNumber);
